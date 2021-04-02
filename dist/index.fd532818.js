27049f507d784ff90a9f8b032b698157426715bd
Advait Kalakkad <advait.kalakkad@gmail.com>
2021-04-02 07:43:36 -0700

    basic night mode

--- dist/index.fd532818.js
+++ dist/index.fd532818.js
@@ -446,13 +446,29 @@ let html = require('choo/html');
 let choo = require('choo');
 
 let app = choo();
+app.use(state);
 app.route('/', home);
 app.route('/hypercore', hypercore);
 app.route('/other', other);
 
-function home() {
+
+function state(state, emitter) {
+    state.nightmode = false;
+    emitter.on('DOMContentLoaded', () => {
+        emitter.on('toggle', data => {
+            if(data == 'n') {
+                state.nightmode = !state.nightmode;
+                console.log(state.nightmode);
+            }
+
+            emitter.emit('render');
+        })
+    })
+}
+
+function home(state, emit) {
     return html`
-    <body onload="${networkConnection}">
+    <body onkeypress="${e => { emit('toggle', e.key)} }" class="${state.nightmode ? "night" : "day"}">
     <div>
         Advait Kalakkad
         <div style="opacity: .8;">
@@ -468,11 +484,11 @@ function home() {
             <li><a href="/other" >Other works</a></li>
         </ul>
         <ul>
-        <li style="font-weight: 400;">Links</li>
-        <li>--------------</li>
-        <li><a href="https://are.na/advait-kalakkad/">Are.na</a></li>
-        <li><a href="https://github.com/advino/">Github</a></li>
-    </ul>
+            <li style="font-weight: 400;">Links</li>
+            <li>--------------</li>
+            <li><a href="https://are.na/advait-kalakkad/">Are.na</a></li>
+            <li><a href="https://github.com/advino/">Github</a></li>
+        </ul>
     </div>
     <div>
         Designer by day ☀
@@ -489,7 +505,7 @@ function hypercore() {
             <div>
                 Hypercore Experiements
             </div>
-            <div>
+            <div class="half">
                 In learning about the p2p space and the design and development of distributed applications, I have built a series of small experiements to test the capabilies and better understand the Hypercore framework.
             </div>
 
@@ -514,16 +530,20 @@ function other() {
     `
 }
 
-function networkConnection() {
+app.mount('body');
 
-    let geo = navigator.geolocation;
-
-    geo.getCurrentPosition(loc => {
-        console.log(loc);
-    });
+function toggleNight(e) {
+    if(e.key === 'n') {
+        console.log('n')
+        emit('toggle');
+    }
 }
 
-app.mount('body');
+// window.addEventListener('keypress', e => {
+//     if(e.key === 'n') {
+//         emit('toggle');
+//     }
+// })
 },{"choo/html":"6rR1q","choo":"5dj5z"}],"6rR1q":[function(require,module,exports) {
 module.exports = require('nanohtml')
 
