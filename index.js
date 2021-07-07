6ee92c3cbc76c8dd6f3bbc432c8248f1078d5429
Advait Kalakkad <advait.kalakkad@gmail.com>
2021-07-07 14:01:35 -0700

    Adding better bio, improving build & serve scripts

--- index.js
+++ index.js
@@ -190,12 +190,20 @@ function about() {
                 </div>
 
                 <div>
+                    <span class="text">
+                        Currently designing and building peer-to-peer objects & software. Researching connected objects, daemons, and agents that collect, maintain, and repair. Believer of the crossing of <a href="https://youtube.com/clip/UgzqNF4tAyUaxZUlpiV4AaABCQ">smart and stupid</a>.
+                    </span>
+                </div>
+
+                <div>
+                    <span class="text">A Brief History</span>
+                    <br>
+                    <br>
                     <span class="text">2019 ${"->"} Google  </span>
                     <br>
                     <span class="text">2015 ${"->"} 2019 RISD BFA Industrial Design</span>
                 </div>
             </div>
-
         </div>
     `
 }
